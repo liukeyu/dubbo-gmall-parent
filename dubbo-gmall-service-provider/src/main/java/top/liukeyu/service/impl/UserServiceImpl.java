d950c51c79f71f3823fecc95231dbd73ff0bc50b
liukeyu <liukeyu@live.com>
2019-08-14 00:01:14 +0800

    change module name

--- dubbo-gmall-service-provider/src/main/java/top/liukeyu/service/impl/UserServiceImpl.java
+++ dubbo-gmall-service-provider/src/main/java/top/liukeyu/service/impl/UserServiceImpl.java
@@ -8,9 +8,9 @@ public class UserServiceImpl  implements UserService {
         // TODO Auto-generated method stub
         System.out.println("服务端被调用");
         UserAddress userAddress = new UserAddress();
-        userAddress.setAddress("北京市东城区89号");
-        userAddress.setName("科比");
-        userAddress.setPhone("187777737737");
+        userAddress.setAddress("上海市徐汇区龙田路190号");
+        userAddress.setName("Mike");
+        userAddress.setPhone("19901897543");
         return userAddress;
     }
 
